75e48516e876175181b5124aba0d3a93594947ef
Hexanix <damian.d@abv.bg>
2017-06-03 17:47:13 +0300

    Full emotions

--- EmUzerWeb/Controllers/Spotify/PlaylistController.cs
+++ EmUzerWeb/Controllers/Spotify/PlaylistController.cs
@@ -20,7 +20,167 @@ namespace EmUzerWeb.Controllers.Spotify
             return View();
         }
 
-        public ActionResult Sad()
+        public ActionResult Happiness() 
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "6OqhFYFJDnBBHas02HopPT" },
+                new List<string>() { "pop", "dance" },
+                new List<string>() { "3LuLUNgRmmgcr14dTIWH2S" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        } 
+
+
+        public ActionResult Anger()
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "5RADpgYLOuS2ZxDq7ggYYH", "30U8fYtiNpeA5KH6H87QUV" },
+                new List<string>() { "punk-rock", "heavy-metal" },
+                new List<string>() { "5iAet1Smzk6SWMBR6eWNBz" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+        public ActionResult Contempt() 
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "0wNZvrIMNUCs24G0wFg2D6", "6A7uqgC2N1nUhrCLAytHxN" },
+                new List<string>() { "punk-rock", "pop-punk" },
+                new List<string>() { "4YX32mhnhrnbUeeTe846it" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+        public ActionResult Surprise() //unfinished
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "0wNZvrIMNUCs24G0wFg2D6", "1aDpQ3bo57IlYWmsG5sdlp" },
+                new List<string>() { "noise-pop", "noise-rock" },
+                new List<string>() { "4J5htL1ewHiMh3c60U5Ddx" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+
+        public ActionResult Fear() //unfinished
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "6uVeWolwyRDrT84lLsaZyW" },
+                new List<string>() { "noise", "blackgaze" },
+                new List<string>() { "1yDMqzf9oZzjn2UJtaQAuP" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+
+
+        public ActionResult Disgust()
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "655AEE6ifgDJ3TYkhHdP48", "" },
+                new List<string>() { "doom-metal"},
+                new List<string>() { "34dkZZNQJzEJRqPkywYmEY" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+
+        public ActionResult Sadness()
         { 
             SpotifyWebAPI spotify = new SpotifyWebAPI
             {
@@ -28,6 +188,19 @@ namespace EmUzerWeb.Controllers.Spotify
                 AccessToken = Session["SpotifyToken"].ToString(),
                 TokenType = "Bearer",
             };
+
+            //TuneableTrack min = new TuneableTrack()
+            //{
+            //    Danceability = 0.562f,
+            //    Energy = 0.187f,
+            //    Liveness = 0.132f,
+            //};
+            //TuneableTrack max = new TuneableTrack()
+            //{
+            //    Danceability = 0.438f,
+            //    Energy = 0.940f,
+            //    Liveness = 0.300f,
+            //};
 
             var recommendedSongs = spotify.GetRecommendations(
                 new List<string>(){ "5VnYwYnG7QmpzQtxyubIwh" }, 
@@ -44,5 +217,35 @@ namespace EmUzerWeb.Controllers.Spotify
 
             return View();
         }
+
+
+        public ActionResult Neutral()
+        {
+            SpotifyWebAPI spotify = new SpotifyWebAPI
+            {
+                UseAuth = true,
+                AccessToken = Session["SpotifyToken"].ToString(),
+                TokenType = "Bearer",
+            };
+
+            var recommendedSongs = spotify.GetRecommendations(
+                new List<string>() { "1HY2Jd0NmPuamShAr6KMms", "1Xyo4u8uXC1ZmMpatF05PJ" },
+                new List<string>() { "pop"},
+                new List<string>() { "6I9VzXrHxO9rA9A5euc8Ak" },
+                null, null, null, 50, "BG");
+
+            var seededTracksUris = new List<string>();
+            recommendedSongs.Tracks.ForEach(item => seededTracksUris.Add(item.Uri));
+
+            var playlist = spotify.CreatePlaylist(this.Session["UserId"].ToString(), "SadTest");
+            spotify.FollowPlaylist(this.Session["UserId"].ToString(), playlist.Id);
+            spotify.AddPlaylistTracks(this.Session["UserId"].ToString(), playlist.Id, seededTracksUris);
+
+            return View();
+        }
+
+
+
+
     }
 }
