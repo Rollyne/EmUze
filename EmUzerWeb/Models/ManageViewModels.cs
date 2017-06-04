6af142807de7dbc1ac055927ba56f80fad30ea28
Rollyne <rollyne@abv.bg>
2017-06-04 11:08:36 +0300

    Work on manage controller

--- EmUzerWeb/Models/ManageViewModels.cs
+++ EmUzerWeb/Models/ManageViewModels.cs
@@ -7,6 +7,8 @@ namespace EmUzerWeb.Models
 {
     public class IndexViewModel
     {
+        public string ProfilePicturePath { get; set; }
+        public string Username { get; set; }
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
